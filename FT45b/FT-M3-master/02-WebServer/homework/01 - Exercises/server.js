03209945ed90b77a5e997c4fa1cc2d7c30917d24
danielmejia1985 <daniel.mejiapalacios1985@gmail.com>
2023-12-30 23:41:33 -0600

    homework wenserver terminada

--- FT45b/FT-M3-master/02-WebServer/homework/01 - Exercises/server.js
+++ FT45b/FT-M3-master/02-WebServer/homework/01 - Exercises/server.js
@@ -1,10 +1,46 @@
-var fs = require("fs");
-var http = require("http");
+const fs = require("fs");
+const http = require("http");
+
 /* ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️ */
 /* AQUÍ DEBAJO PUEDES ESCRIBIR LA CONSTANTE DEL PUERTO */
-const PORT = null;
+const PORT = 3001;
+
+const firstServer =  http.createServer((request, response) => {
+  console.log(`Server raised in port ${PORT}`);
+  switch (request.url) {
+    case "/api":
+        fs.readFile("./utils/dogsData.json", (err, data) => {
+          if (err) {
+            response.writeHead(404, {"Content-Type": "text/plain"})
+            response.end("json not found")
+            return
+          }
+            response.writeHead(200, {"Content-Type": "application/json"})
+            response.end(data)
+        })        
+      break;
+
+    case "/allDogs":
+          fs.readFile("./utils/allDogs.html", "UTF8", (err, data) => {
+            if (err) {
+              response.writeHead(404, {"Content-Type": "text/plain"})
+              response.end("html not found")
+              return
+            }
+              response.writeHead(200, {"Content-Type": "text/html"})
+              response.end(data)
+          })
+      break
+  
+    default:
+      response.writeHead(404, {"Content-Type": "text/plain"})
+      response.end("Route not found")
+      break;
+  }
+
+}).listen(PORT, "127.0.0.1");
 
 /* ⚠️ LA LÍNEA SIGUIENTE TIENE QUE QUEDAR COMO ESTÁ PARA PODER EXPORTAR EL SERVIDOR ⚠️ */
-module.exports =
-  /* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
-  null;
+/* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
+module.exports = firstServer;
+  
--- FT45b/FT-M3-master/02-WebServer/homework/01 - Exercises/tests/02.test.js
+++ FT45b/FT-M3-master/02-WebServer/homework/01 - Exercises/tests/02.test.js
@@ -5,11 +5,14 @@ const fs = require("fs");
 describe("02 | Ejercicios", () => {
   it("GET /api, debe responder con un error o con un json", async () => {
     const response = await request(server).get("/api");
+    console.log(response);
     if (response.statusCode === 200) {
       expect(response.type).toBe("application/json");
       expect(JSON.parse(response.text)).toEqual(
         JSON.parse(fs.readFileSync("./utils/dogsData.json"))
       );
+      // console.log(response.text);
+      // console.log(typeof response.text);
     } else {
       expect(response.statusCode).toBe(404);
       expect(response.text).toBe("json not found");
